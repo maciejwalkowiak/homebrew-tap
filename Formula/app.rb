310f02d0ac09cdb1441593eac20c21d9c7dd9117
jreleaserbot <jreleaser@kordamp.org>
2022-11-08 11:24:21 +0100

    app v0.1.0

--- Formula/app.rb
+++ Formula/app.rb
@@ -1,20 +1,17 @@
-# Generated with JReleaser 1.3.1 at 2022-11-08T11:14:20.75529+01:00
 class App < Formula
   desc "yolo"
   homepage "https://github.com/maciejwalkowiak/yolo"
   url "https://github.com/maciejwalkowiak/yolo/releases/download/v0.1.0/yolo-amd64-darwin.zip"
   version "0.1.0"
-  sha256 "144a9c52e157ea6d24eecb42dd78906ed086361872a38aaa5520fba5aa00a273"
+  sha256 "6a16cc0984741098ad07f8bbe88c7055cd8d3ca7c8495a1bdd516cce876283e7"
   license "MIT"
 
 
   def install
-    libexec.install Dir["*"]
-    bin.install_symlink "#{libexec}/bin/app" => "app"
+    bin.install "yolo-amd64-darwin" => "yolo"
   end
 
   test do
-    output = shell_output("#{bin}/app --version")
-    assert_match "0.1.0", output
+    system "false"
   end
 end

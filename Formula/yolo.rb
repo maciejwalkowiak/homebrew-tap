26376f81415f3f1c77df0b083ba391c4a26c3efa
jreleaserbot <jreleaser@kordamp.org>
2024-03-29 12:03:38 +0100

    yolo v0.3.2

--- Formula/yolo.rb
+++ Formula/yolo.rb
@@ -1,9 +1,9 @@
 class Yolo < Formula
   desc "yolo"
   homepage "https://github.com/maciejwalkowiak/yolo"
-  url "https://github.com/maciejwalkowiak/yolo/releases/download/v0.3.1/yolo-amd64-darwin.zip"
-  version "0.3.1"
-  sha256 "2097968fee1aac86214b976e477c1e5cff46b80365e83617e8ebd19707188996"
+  url "https://github.com/maciejwalkowiak/yolo/releases/download/v0.3.2/yolo-amd64-darwin.zip"
+  version "0.3.2"
+  sha256 "e63bae7facffe30955d324aa9e89d67c8dc6ce931eaaa425bfd0822be6066621"
   license "MIT"
 
 
